b9811403ab8316839238ebe652f1b57111d613d0
silverjoda <heizen.sh@gmail.com>
2022-01-26 14:08:13 +0100

    Add cvx

--- policies/policies.py
+++ policies/policies.py
@@ -5,6 +5,20 @@ from torch.nn.utils import weight_norm
 import cvxpy as cp
 from cvxpylayers.torch import CvxpyLayer
 
+class SLP(nn.Module):
+    def __init__(self, obs_dim, act_dim, hid_dim=128):
+        super(SLP, self).__init__()
+        self.obs_dim = obs_dim
+        self.act_dim = act_dim
+        self.hid_dim = hid_dim
+
+        self.fc1 = T.nn.Linear(self.obs_dim, self.act_dim, bias=True)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        return out
+
+
 class MLP(nn.Module):
     def __init__(self, obs_dim, act_dim, hid_dim=128):
         super(MLP, self).__init__()
@@ -22,6 +36,7 @@ class MLP(nn.Module):
         out = self.fc3(fc2)
         return out
 
+
 class CVX(nn.Module):
     def __init__(self, obs_dim, act_dim, hid_dim=128):
         super(CVX, self).__init__()
@@ -32,14 +47,15 @@ class CVX(nn.Module):
         self.fc_in = T.nn.Linear(self.obs_dim, self.hid_dim, bias=True)
         self.fc_out = T.nn.Linear(self.hid_dim, self.act_dim, bias=True)
 
-        _x = cp.Parameter(self.hid_dim)
-        _y = cp.Variable(self.hid_dim)
-        obj = cp.Minimize(-_x.T @ _y - cp.sum(cp.entr(_y)))
+        self._x = cp.Parameter(self.hid_dim)
+        self._y = cp.Variable(self.hid_dim)
+        obj = cp.Minimize(-self._x.T @ self._y - cp.sum(cp.entr(self._y)))
         prob = cp.Problem(obj)
-        self.cvx_layer = CvxpyLayer(prob, parameters=[_x], variables=[_y])
+        self.cvx_layer = CvxpyLayer(prob, parameters=[self._x], variables=[self._y])
 
     def forward(self, x):
-        x_in = T.tanh(self.fc_in(x))
+        x_in = self.fc_in(x)
         cvx_out, = self.cvx_layer(x_in)
         out = self.fc_out(cvx_out)
+
         return out
